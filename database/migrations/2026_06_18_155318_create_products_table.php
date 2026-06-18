72b6ed2cd60191b261cd2a242763162d08081493
LumiNovryM <luminovrymekel@gmail.com>
2026-06-18 23:00:03 +0700

    feat: create model, migrations, and relationship entity

---
+++ database/migrations/2026_06_18_155318_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->decimal('price', 12, 2);
+            $table->decimal('flash_sale_price', 12, 2)
+                ->nullable();
+            $table->integer('inventory');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
